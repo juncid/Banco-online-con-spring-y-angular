5f5d3cb5787cdd6281742b3ce31a56080e6f63e5
juncid <juncid@gmail.com>
2020-03-17 20:25:28 -0300

    ADD: add static files for html. ADD: add index. ADD: add common header. Add: add signup Controller and View.

---
+++ src/main/java/com/userfront/controller/HomeController.java
@@ -0,0 +1,36 @@
+package com.userfront.controller;
+
+import com.userfront.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "redirect:/index";
+    }
+
+    @RequestMapping("/index")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup(Model model) {
+        User user = new User();
+
+        model.addAttribute("user", user);
+
+        return "signup";
+    }
+
+
+}
