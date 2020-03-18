98fa32141b7dad6b57ea39d4937f0e91fe7fbbec
juncid <juncid@gmail.com>
2020-03-18 20:39:16 -0300

    SETUP: add propierties for Postgresql DB. MODEL: Configure models for banking app.

--- src/main/java/com/userfront/domain/User.java
+++ src/main/java/com/userfront/domain/User.java
@@ -1,21 +1,43 @@
 package com.userfront.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.HashSet;
 import java.util.List;
 
+@Entity
+@Table(name="user_bank")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "userId", nullable = false, updatable = false)
     private Long userId;
+
     private String username;
     private String password;
     private String firstName;
     private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
+
     private String phone;
 
     private boolean enabled=true;
 
+    @OneToOne
     private PrimaryAccount primaryAccount;
+
+    @OneToOne
     private SavingsAccount savingAccount;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
     private List<Appointment> appointmentList;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Recipient> recipientList;
 
     public Long getUserId() {
